4e85e153cead8e02eca631b202a50fd3ef5e0bd2
ronaldiputra09 <emailnyaronal@gmail.com>
2024-10-28 08:50:38 +0700

    add : initial get_storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:test_rimba/app/data/themes.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
   runApp(
     GetMaterialApp(
       title: "Rimba Apps",
